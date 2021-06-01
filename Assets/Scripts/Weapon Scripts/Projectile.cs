082d3529793b243bb0ccc74b74c61b5361b8e684
Dinis Costa <scorpaust@hotmail.com>
2021-06-01 12:40:18 +0100

    Created projectiles with visual and sound effects.

---
+++ Assets/Scripts/Weapon Scripts/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 3f;
+
+    public float minDamage = 10f;
+
+    public float maxDamage = 30f;
+
+    private float projectileDamage;
+
+    [SerializeField]
+    private AudioClip spawnSound;
+
+    [SerializeField]
+    private AudioClip destroySound;
+
+    [SerializeField]
+    private GameObject boomEffect;
+
+	private void Start()
+	{
+        projectileDamage = (int)Random.Range(minDamage, maxDamage);
+
+        if (spawnSound)
+            AudioSource.PlayClipAtPoint(spawnSound, new Vector3(0f, 6f, 0f));
+	}
+
+	private void Update()
+	{
+        transform.Translate(0f, speed * Time.deltaTime, 0f);
+	}
+
+}
