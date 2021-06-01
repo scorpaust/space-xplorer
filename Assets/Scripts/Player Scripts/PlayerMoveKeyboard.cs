c86ef695e19ed8615b50b2dbe1ee6d637ca4d0a4
Dinis Costa <scorpaust@hotmail.com>
2021-06-01 11:59:22 +0100

    Handled player movement and fire engines emission.

---
+++ Assets/Scripts/Player Scripts/PlayerMoveKeyboard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveKeyboard : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 600f;
+
+    private Rigidbody2D myBody;
+
+
+	private void Awake()
+	{
+        myBody = GetComponent<Rigidbody2D>();
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	private void FixedUpdate()
+	{
+		HandleMovement();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+	private void HandleMovement()
+	{
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+			myBody.AddForce(transform.up * speed);
+
+		if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+			myBody.AddForce(-transform.up * speed);
+
+		if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+			myBody.AddForce(transform.right * speed);
+
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+			myBody.AddForce(-transform.right * speed);
+	}
+}
