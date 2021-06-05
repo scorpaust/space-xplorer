980ab18336f327dd7edf67876a8a37418fdc47bd
Dinis Costa <scorpaust@hotmail.com>
2021-06-05 18:00:07 +0100

    Handled enemy movement.

---
+++ Assets/Scripts/Enemy Scripts/EnemyMovementInPoints.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementInPoints : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] movementPoints;
+
+    [SerializeField]
+    private float moveSpeed = 8f;
+
+    [SerializeField]
+    private bool moveRandomly;
+
+    private int currentMoveIndex;
+
+    private Vector3 targetPos;
+
+	private void Start()
+	{
+        SetTargetPosition();
+	}
+
+	private void Update()
+	{
+        Move();
+	}
+
+	private void SelectRandomPosition()
+	{
+        while (movementPoints[currentMoveIndex].position == targetPos)
+            currentMoveIndex = Random.Range(0, movementPoints.Length);
+
+        targetPos = movementPoints[currentMoveIndex].position;
+	}
+
+    private void SelectPointToPointPosition()
+	{
+        if (currentMoveIndex == movementPoints.Length)
+            currentMoveIndex = 0;
+
+        targetPos = movementPoints[currentMoveIndex].position;
+
+        currentMoveIndex++;
+    }
+
+    private void SetTargetPosition()
+	{
+        if (moveRandomly)
+            SelectRandomPosition();
+        else
+            SelectPointToPointPosition();
+	}
+
+    private void Move()
+	{
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+		{
+            SetTargetPosition();
+		}
+	}
+
+}
---
+++ Assets/Scripts/Enemy Scripts/EnemyMovementRandom.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementRandom : MonoBehaviour
+{
+    [SerializeField]
+    private float minX, maxX;
+
+    [SerializeField]
+    private float minY, maxY;
+
+    [SerializeField]
+    private float moveSpeed = 8f;
+
+    private Vector3 targetPos;
+
+	private void Start()
+	{
+        SetTargetPosition();
+	}
+
+	private void Update()
+	{
+        Move();
+	}
+
+	private void SetTargetPosition()
+	{
+        targetPos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+	}
+
+    private void Move()
+	{
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+            SetTargetPosition();
+
+	}
+}
