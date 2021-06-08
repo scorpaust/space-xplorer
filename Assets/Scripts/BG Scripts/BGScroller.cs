800cb1bf04700f294e0b1f47aa2f0508a6ac3ba7
Dinis Costa <scorpaust@hotmail.com>
2021-06-08 20:03:01 +0100

    BG Scroller.

---
+++ Assets/Scripts/BG Scripts/BGScroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0.1f;
+
+    private float yAxis;
+
+    private Material bgMaterial;
+
+	private void Awake()
+	{
+		bgMaterial = GetComponent<Renderer>().material;
+	}
+
+	private void Update()
+	{
+		yAxis += speed * Time.deltaTime;
+
+		bgMaterial.SetTextureOffset("_MainTex", new Vector2(0f, yAxis));
+	}
+}
